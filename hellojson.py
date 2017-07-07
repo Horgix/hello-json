580566fcb2663e0c2d7e198ce34e704d07e843d5
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-07-07 10:24:03 +0200

    Change world to world2

--- hellojson.py
+++ hellojson.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return jsonify({'hello': 'world'})
+    return jsonify({'hello': 'world2'})
 
 if __name__ == '__main__':
     app.run(debug=False)
